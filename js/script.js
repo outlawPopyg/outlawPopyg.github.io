5deeb8fb5a7746ecef94445a45236c071b9f3379
Popyg <tyl9ray@yandex.ru>
2021-07-30 20:11:11 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ close.addEventListener("click", () => menu.classList.remove('active'));
 menuOverlay.addEventListener('click', ({target}) => menu.classList.remove('active'));
 
 const progressBars = document.querySelectorAll('[percent]');
-progressBars.forEach(el => el.style.width = el.getAttribute('percent') * 350 / 100 + 'px');
+const windowWidth = window.outerWidth;
 
+
+progressBars.forEach(el => {
+    el.style.width = el.getAttribute('percent') * (windowWidth > 437 ? 350 : 300) / 100 + 'px';
+});
 
 
