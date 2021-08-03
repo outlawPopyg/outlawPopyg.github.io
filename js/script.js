8634a68d5f44a01d8989948308ea0abae9295cfe
Popyg <tyl9ray@yandex.ru>
2021-08-03 22:18:09 +0300

    final versrion

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const burger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu__close'),
-      menuOverlay = document.querySelector('.menu__overlay');
+      menuOverlay = document.querySelector('.menu__overlay'),
+      menuLink = document.querySelectorAll('.menu .menu__list li');
 
 burger.addEventListener("click", () => menu.classList.add('active'));
 close.addEventListener("click", () => menu.classList.remove('active'));
@@ -10,6 +11,9 @@ menuOverlay.addEventListener('click', ({target}) => menu.classList.remove('activ
 const progressBars = document.querySelectorAll('[percent]');
 const windowWidth = window.outerWidth;
 
+menuLink.forEach(el => {
+    el.addEventListener('click', () => menu.classList.remove('active'));
+});
 
 progressBars.forEach(el => {
     el.style.width = el.getAttribute('percent') * (windowWidth > 437 ? 350 : 300) / 100 + 'px';
